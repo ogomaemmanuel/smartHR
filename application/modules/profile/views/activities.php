70c8d3d206a02d032a4422c0a9caffa5c08a7b77
Benz rexlin <benz@dreamguys.co.in>
2018-05-29 19:31:27 +0530

    worked in activity page

--- application/modules/profile/views/activities.php
+++ application/modules/profile/views/activities.php
@@ -1,44 +1,47 @@
-<div class="content">
+
+<div class="content container-fluid">
+	<div class="col-xs-12">
+		<h4 class="page-title"><?=lang('activities')?></h4>
+	</div>
+	
 	<div class="row">
-		<div class="col-xs-12">
-			<h4 class="page-title"><?=lang('activities')?></h4>
+		<div class="col-md-12">
+			<div class="activity">
+				<div class="activity-box">
+					<ul class="activity-list">
+						<?php foreach (User::user_log(User::get_id()) as $key => $a) { 
+							?>
+							<li>								
+								<div class="activity-user">
+									<a class="avatar" href="javascript:void(0);">
+										<img src="<?php echo User::avatar_url($a->user);?>" class="img-circle">
+									</a>
+								</div>
+								<div class="activity-content">
+									<div class="timeline-content">
+										<a><?=User::displayName($a->user)?></a>&nbsp;&nbsp;
+										<a><?=strtoupper($a->module)?></a>&nbsp;&nbsp;
+										<a>
+											<?php 
+											if (lang($a->activity) != '') {
+												if (!empty($a->value1)) {
+													if (!empty($a->value2)){
+														echo sprintf(lang($a->activity), '<em>'.$a->value1.'</em>', '<em>'.$a->value2.'</em>');
+													} else {
+														echo sprintf(lang($a->activity), '<em>'.$a->value1.'</em>');
+													}
+												} else { echo lang($a->activity); }
+											} else { echo $a->activity; } 
+											?>
+											</a>
+											<span class="time"><?php echo Applib::time_elapsed_string(strtotime($a->activity_date));?></span>
+										</div>
+									</div>
+								</li>
+								<?php } ?>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-	<div class="table-responsive">
-		<table id="table-activities" class="table table-striped custom-table m-b-0 AppendDataTables">
-			<thead>
-				<tr>	
-					<th><?=lang('activity_date')?></th>	
-					<th><?=lang('user')?></th>			
-					<th><?=lang('module')?></th>
-					<th><?=lang('activity')?> </th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach (User::user_log(User::get_id()) as $key => $a) { ?>
-				<tr>
-					<td><?=$a->activity_date?></td>
-					<td><?=User::displayName($a->user)?></td>
-					<td><?=strtoupper($a->module)?></td>
-					<td>
-						<?php 
-						if (lang($a->activity) != '') {
-							if (!empty($a->value1)) {
-								if (!empty($a->value2)){
-									echo sprintf(lang($a->activity), '<em>'.$a->value1.'</em>', '<em>'.$a->value2.'</em>');
-								} else {
-									echo sprintf(lang($a->activity), '<em>'.$a->value1.'</em>');
-								}
-							} else { echo lang($a->activity); }
-						} else { echo $a->activity; } 
-						?>
-						<?php if (config_item('last_seen_activities') < strtotime($a->activity_date)) { ?>
-						&nbsp;&nbsp;<span class="badge bg-success text-white"><?=lang('new')?></span>
-						<?php } ?>
-					</td>
-				</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-	</div>
-</div>
